889493a52e98a0a3a3fc303ecb20d12ef1e4e81e
Tristan Elain <tristan.elain@gmail.com>
2019-02-28 19:52:56 +0100

    Add main function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const {Photo, Slide} = require('./classes');
 
-const photo = new Photo(1, 'V', ['toto']);
-
-console.log(photo.tags);
+function main() {
+    const files = [
+        './subject/a_example.txt',
+        './subject/b_lovely_landscapes.txt',
+        './subject/c_memorable_moments.txt',
+        './subject/d_pet_pictures.txt',
+        './subject/e_shiny_selfies.txt',
+    ];
+}
