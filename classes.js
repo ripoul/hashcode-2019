09dad2de27d5ce3e8f62757a4efe8421a041b91e
Tristan Elain <tristan.elain@gmail.com>
2019-02-28 19:37:39 +0100

    Add methods to Photo

--- classes.js
+++ classes.js
@@ -1,16 +1,26 @@
+const VERTICAL = 'V', HORIZONTAL = 'H';
+
 class Photo {
-    constructor(id, orientation, tags) {
-        this.id = id;
-        this.orientation = orientation;
-        this.tags = tags;
-    }
+  constructor(id, orientation, tags) {
+    this.id = id;
+    this.orientation = orientation;
+    this.tags = tags;
+  }
+
+  isVertical() {
+    return this.orientation === VERTICAL;
+  }
+
+  isHorizontal() {
+    return this.orientation === HORIZONTAL;
+  }
 }
 
 class Slide {
-    constructor(photoIds, tags) {
-        this.photoIds = photoIds;
-        this.tags = tags;
-    }
+  constructor(photoIds, tags) {
+    this.photoIds = photoIds;
+    this.tags = tags;
+  }
 }
 
 module.exports.Photo = Photo;
