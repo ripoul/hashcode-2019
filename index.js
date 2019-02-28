b8e12519bc4306211d4a9756ba22cba2ed2acbe1
Tristan Elain <tristan.elain@gmail.com>
2019-02-28 21:38:16 +0100

    Because Jules (@ripoul) is worried

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function main() {
     './subject/d_pet_pictures.txt',
     './subject/e_shiny_selfies.txt',
   ];
-  const photo = initSession(files[0]);
+  const photos = initSession(files[0]);
+  const slides = createSlide(photos);
+  
 }
 
 function tags_in_common(slide1, slide2) {
@@ -23,30 +25,32 @@ function have_tags_in_common(slide1, slide2) {
 }
 
 function find_slide_with_hashtag_in_common(slides, slide) {
-    return slides.filter(s => have_tags_in_common(s, slide));
+  return slides.filter(s => have_tags_in_common(s, slide));
 }
 
 function getSlideWithMoreCommonTags(slides, slide) {
-    let bestFound = {index: 0, tags: 0}
-    slides.map((value, index) => {
-        const tags = tags_in_common(slide, value);
-        if (bestFound.tags < tags) {
-            bestFound = {index, tags};
-        }
-    });
-    return bestFound.tags ? slides[bestFound.index] : null;
+  let bestFound = {index: 0, tags: 0};
+  slides.map((value, index) => {
+    const tags = tags_in_common(slide, value);
+    if (bestFound.tags < tags) {
+      bestFound = {index, tags};
+    }
+  });
+  return bestFound.tags ? slides[bestFound.index] : null;
 }
 
-getPhotoWithLessCommonTags(photos, photo, maxTags) {
-    let best = { photo: null, tags: maxTags }
-    for (let ph of photo) {
-        const tags = tags_in_common(photo, ph);
-        if (tags < best.tags) {
-            best = { photo: ph, tags };
-            if (!tags) { continue; }
-        }
+function getPhotoWithLessCommonTags(photos, photo, maxTags) {
+  let best = {photo: null, tags: maxTags};
+  for (let ph of photos) {
+    const tags = tags_in_common(photo, ph);
+    if (tags < best.tags) {
+      best = {photo: ph, tags};
+      if (!tags) {
+        continue;
+      }
     }
-    return best.photo;
+  }
+  return best.photo;
 }
 
 function initSession(filePath) {
@@ -58,55 +62,56 @@ function initSession(filePath) {
 }
 
 function return_comme_c_attendu(slides) {
-    let toWrite = ""
-    toWrite += slides.length +"\n"
-    slides.forEach(slide => {
-        let line = "";
-        slide.photoIds.forEach(id => {
-            line += id + " ";
-        });
-        line+="\n"
-        toWrite += line;
+  let toWrite = ''
+  toWrite += slides.length + '\n'
+  slides.forEach(slide => {
+    let line = '';
+    slide.photoIds.forEach(id => {
+      line += id + ' ';
     });
-    fs.writeFile("retFile.txt", toWrite);
+    line += '\n'
+    toWrite += line;
+  });
+  fs.writeFile('retFile.txt', toWrite);
 }
 
-function createSlide() {
-	var horizontal = [];
-	var vertical = [];
-	var slide = [];
-	var used = [];
-	var max = 0;
+function createSlide(photos) {
+  var horizontal = [];
+  var vertical = [];
+  var slide = [];
+  var used = [];
+  var max = 0;
 
-	//sépare vertical et horizontal
-	photos.forEach(element => {
-		if(element.tag.length > max) {
-			max = element.tag.length;
-		}
-		if(element.orientation == 'H') {
-			horizontal.push(element);
-		} else {
-			vertical.push(element);
-		}
-	});
-	
-	//ajoute photo h à sortie
-	horizontal.forEach(element => {
-		slide.push(new Slide([element.id], [element.tags]));
-	});
+  // sépare vertical et horizontal
+  photos.forEach(element => {
+    if (element.tags.length > max) {
+      max = element.tags.length;
+    }
+    if (element.orientation == 'H') {
+      horizontal.push(element);
+    } else {
+      vertical.push(element);
+    }
+  });
 
-	//ajoute photo v à la sortie
-	vertical.map((photo) => {
-		if(!used.includes(photo)) {
-			
-			var bestPhoto = getPhotoWithLessCommonTags(vertical, photo, max);
-			
-			slide.push(new Slide([photo.id, bestPhoto.id], new Set([photo.tags, bestPhoto.tags])));
+  // ajoute photo h à sortie
+  horizontal.forEach(element => {
+    slide.push(new Slide([element.id], [element.tags]));
+  });
 
-			used.push(bestPhoto);
-		}
-	});
-	return slide;
+  // ajoute photo v à la sortie
+  vertical.map((photo) => {
+    if (!used.includes(photo)) {
+      var bestPhoto = getPhotoWithLessCommonTags(vertical, photo, max);
+
+      slide.push(new Slide(
+          [photo.id, bestPhoto.id],
+          Array.from(new Set(photo.tags.concat(bestPhoto.tags)))));
+
+      used.push(bestPhoto);
+    }
+  });
+  return slide;
 }
 
 main();
