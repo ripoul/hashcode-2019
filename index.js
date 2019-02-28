640e7eff550396778e8ccc32eaf4da204b963fcf
Jules LE BRIS <jls.lebris@gmail.com>
2019-02-28 20:16:16 +0100

    add les fonctions les plus utiles

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function main() {
         './subject/e_shiny_selfies.txt',
     ];
 }
+
+function tags_in_common(slide1, slide2) {
+    intersec = slide1.tags.filter(value => -1 !== slide2.tags.indexOf(value));
+    return intersec.length
+}
+
+function have_tags_in_common(slide1, slide2) {
+    return tags_in_common > 0
+}
+
+function find_slide_with_hashtag_in_common(slides, slide) {
+    ret = Array();
+    slides.forEach(s => {
+        if (have_tags_in_common(s, slide)) {
+            ret.push(slide)
+        }
+    });
+    return ret;
+}
