b605eb3454fb0e8197610af7b4a48ccdb9bc7a0e
Tristan Elain <tristan.elain@gmail.com>
2019-02-28 20:10:45 +0100

    Implements function that create all photos

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const fs = require('fs');
 const {Photo, Slide} = require('./classes');
 
 function main() {
-    const files = [
-        './subject/a_example.txt',
-        './subject/b_lovely_landscapes.txt',
-        './subject/c_memorable_moments.txt',
-        './subject/d_pet_pictures.txt',
-        './subject/e_shiny_selfies.txt',
-    ];
+  const files = [
+    './subject/a_example.txt',
+    './subject/b_lovely_landscapes.txt',
+    './subject/c_memorable_moments.txt',
+    './subject/d_pet_pictures.txt',
+    './subject/e_shiny_selfies.txt',
+  ];
+
+  const photo = initSession(files[0]);
 }
+
+
+function initSession(filePath) {
+  const content = fs.readFileSync(filePath, 'utf8').split('\r\n');
+  return content.slice(1, -1).map((value, index) => {
+    const splitedValue = value.split(' ');
+    return new Photo(index, splitedValue[0], splitedValue.slice(2));
+  });
+}
+
+main();
